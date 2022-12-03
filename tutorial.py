159a91b05dfe9d791a8b85756130fe70deaf7463
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-03 17:00:30 +0100

    Expressions, statements & programs

--- tutorial.py
+++ tutorial.py
@@ -9,3 +9,18 @@ eatsRice = False
 
 # You can't use a keyword when naming variables, keywords are words that have specific meanings in python e.g are for, if, while, import, and, True, False etc.
 
+# Expressions, Statements & Programs in python - 
+
+# an Expression is any type of code in python that returns a value e.g 
+1 + 1
+"Haruna"
+
+# a Statement is an operation on a value, e.g are;
+ 
+greet = "Hello, world!"
+print(greet)  
+
+# a Program is formed by a series of statement like we have above. But those statements can be put in one line but must be separated by a semi-colon e.g;
+
+greet_two = "Hello, pythonistas!"; print(greet_two)
+

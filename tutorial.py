0520ebc1bd9c633ac13107904500a6d611f85391
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-03 16:58:58 +0100

    Variables & Keywords

---
+++ tutorial.py
@@ -0,0 +1,11 @@
+# Variables - We can create a new variable by assigning a value to a label e.g are as follows;
+
+name = "Alvin"
+age = 32
+likes_food = True
+eatsRice = False 
+
+# Variables can't start with a number and variable names should not be followed with special characters, it's best you use the nnaming convention above when declaring your variables.
+
+# You can't use a keyword when naming variables, keywords are words that have specific meanings in python e.g are for, if, while, import, and, True, False etc.
+

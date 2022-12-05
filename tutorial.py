72468f5b862dec15dbbf0c16b24e6d25d10f5587
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-05 17:43:24 +0100

    String Methods

--- tutorial.py
+++ tutorial.py
@@ -165,3 +165,18 @@ print("""this
         is a multi-line
         string """)
 
+# String Methods - You can perform certain operations or methods on that string. e.g;
+
+print("alvin".upper()) # to make all characters uppercase
+print("ALVIN".lower()) # to make all characters lowercase
+print("ALVIN hARUNA".title()) # to make the first letter of a word uppercase
+print("ALVIN".islower()) # to check if the string is in lowercase, it'll return either True or False
+
+# It is important to know that string methods only returns a new string and doesn't mutate the original string. e.g;
+
+string_method = "check this out"
+print(string_method.title())
+print(string_method)
+
+print(len(string_method)) # check for the length of characters in the string
+print( "eck" in string_method) # using the in operator to check if a string contains a sub string

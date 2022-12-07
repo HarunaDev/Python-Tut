25afd2ca100d3681accd38fdbc9f889a66b20886
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-07 12:42:08 +0100

    added data types lesson

---
+++ 04-Data-Types.py
@@ -0,0 +1,29 @@
+# Data Types - Python has several built in types ranging from strings, numbers, booleans etc. You can check for the type of a data using the type() function. e.g;
+
+# typeCheck = "What data type is this?"
+# print(type(typeCheck))
+# print(isinstance(typeCheck, str))
+
+# in the example above, we are checking if the type of the variable and in the other print statement we are coomparing if the variable type is a string using the isinstance() function.
+
+# Python automatically knows the type of data being stored, but you can also create a variable of a specific type using the class constructor by passing the value or a variable name. it can also be used to convert data types e.g;
+
+# ageCheck = float("32")
+# print(isinstance(ageCheck, float)) 
+# print(ageCheck) 
+
+# below are some data types and how to check them
+
+# complex for complex numbers
+# bool for booleans
+# list for list
+# tuple for tuples
+# range for ranges
+# dict for dictionaries
+# set for sets
+
+# To pass this challenge, you are required to declare 2 variables, the first should be a string and the second should be a float, inside print statements, check for the data type of those variables
+
+# Your code goes below here
+
+# Your code goes above here

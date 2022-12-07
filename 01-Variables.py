f1a9f84b44dd0e86a6a0fe844c68ccca707d8b06
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-07 11:52:37 +0100

    added variable lesson

---
+++ 01-Variables.py
@@ -0,0 +1,19 @@
+# Variables - We can create a new variable by assigning a value to a label e.g are as follows;
+
+# name = "Alvin"
+# age = 32
+# likes_food = True
+# eatsRice = False 
+
+# print(name, "is", age, "years old")
+
+# Variables can't start with a number and variable names should not be followed with special characters, it's best you use the nnaming convention above when declaring your variables.
+
+# You can't use a keyword when naming variables, keywords are words that have specific meanings in python e.g are for, if, while, import, and, True, False etc.
+
+# To pass the test on this challenge, declare the following variables - name and set it's value to your name, country and set it's value to your country. Then print out the text "(Your name) is from (Your country)"
+
+
+# Your code goes below here
+
+# Your code goes above here

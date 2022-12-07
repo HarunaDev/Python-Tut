34ab4b097f18ed9ca4dafa41174ede5cd579da72
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-07 12:17:03 +0100

    added comments & indentation lesson

---
+++ 03-Comments-Indentation.py
@@ -0,0 +1,15 @@
+# Comments - In the python language or program, anything after a hashtag is completely ignored, e.g;
+
+# this is a commented line.
+4 + 4 # this is an inline comment
+
+# Programmers use comments to make code inactive and also pass messages in their programs to other developers who might be reading their code.  
+
+# Indentation - Every line of code indented belongs only to the block of code that it is indented within, it is important to know that indentation is very important in python.
+
+# To pass this challenge you are required to comment out the codes below, the first one should be a commented line and the other one should be an inline comment. 
+
+# Your code goes below here
+comment = "This should be a commented line"
+comment_two = "The inline comment should say 'Inline comments are useful.'"
+# Your code goes above here 

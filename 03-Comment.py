c10e0ae045155b40081ed2b25858480c13d4330d
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-10 01:07:24 +0100

    refactored lesson

---
+++ 03-Comment.py
@@ -0,0 +1,13 @@
+# Comments - In the python language or program, anything after a hashtag is completely ignored, e.g;
+
+# this is a commented line.
+4 + 4 # this is an inline comment
+
+# Programmers use comments to make code inactive and also pass messages in their programs to other developers who might be reading their code.  
+
+# To pass the test on this challenge, you are required to comment out the codes below, the first one should be a commented line and the other one should be an inline comment. 
+
+# Your code goes below here
+comment = "This should be a commented line"
+comment_two = "The inline comment should say 'Inline comments are useful.'"
+# Your code goes above here 

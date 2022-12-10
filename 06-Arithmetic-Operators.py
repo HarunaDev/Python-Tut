6c9db3e5c9e208ef461f4fcff9f28a2936878bf5
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-10 01:39:21 +0100

    Added Arithmetic operator lesson

---
+++ 06-Arithmetic-Operators.py
@@ -0,0 +1,26 @@
+# Arithmetic Operator - They allow us to carry out math expressions in our program eg;
+
+1 + 1 #addition
+2 - 1 #subtraction
+2 * 2 #multiplication
+4 / 2 #division
+4 % 3 #modulus (remainder)
+4 ** 2 #to the power
+5 // 2 #floor division
+
+# we can use the addition operator to concatenate string values. e.g;
+
+# greeting = "Hello," + " " + "Alvin"
+# print(greeting)
+
+# we can also combine arithmetic operators with the assignment operators. eg;
+
+# bamodu_age = 13
+# bamodu_age += 12 # bamodu_age = bamodu_age + 12 
+# print(bamodu_age)
+
+# To pass the test on this challenge, you are required to declare a variable called (Age) and assign it's value to the integer (18), then using the arithmetiic & assignment operator combined, add 20 to the age variable.
+
+# Your code goes below this line
+
+# Your code goes above this line

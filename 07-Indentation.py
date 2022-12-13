8f633ab6d9ded6679bc6bf7d12d1f4174a17e580
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-13 16:01:35 +0100

    added indentation lesson

---
+++ 07-Indentation.py
@@ -0,0 +1,16 @@
+# Indentation - Every line of code indented belongs only to the block of code that it is indented within, it is good to know that indentation is very important in python.
+# The colon character (:) is used to specify Indentation.
+
+# option = input()
+# if option == "Lead-Forte":
+#     print("I go to school here!")
+# else:
+#     print("Sorry, I didn't get it")
+
+# To pass the test on this challenge, declare a variable called (fav_food) and set it's value to an input function call, under that, write an if statement that prints out "That is my favourite food" if (fav_food) is == "Yam", else it should print out the string "Not my favourite food". 
+
+# Note that each print statement should be indented inside of the if/else statements as shown in the example above. 
+
+# Your code goes below this line
+
+# Your code goes above this line

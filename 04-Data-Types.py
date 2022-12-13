c82486c32636972f3d3c78b654692376701efb86
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-14 00:58:54 +0100

    refactored lesson

--- 04-Data-Types.py
+++ 04-Data-Types.py
@@ -6,7 +6,7 @@
 
 # in the example above, we are checking if the type of the variable and in the other print statement we are coomparing if the variable type is a string using the isinstance() function.
 
-# Python automatically knows the type of data being stored, but you can also create a variable of a specific type using the class constructor by passing the value or a variable name. it can also be used to convert data types e.g;
+# Python automatically knows the type of data being stored, but you can also create a variable of a specific type using the class constructor, and you do this by passing the value or a variable name inside brackets. it can also be used to convert data types e.g;
 
 # ageCheck = float("32")
 # print(isinstance(ageCheck, float)) 
@@ -22,7 +22,7 @@
 # dict for dictionaries
 # set for sets
 
-# To pass the test on this challenge, you are required to declare 2 variables, the first should be a string and the second should be a float, inside print statements, check for the data type of those variables
+# To pass the test on this challenge, you are required to declare 2 variables (string_one & string_two), with their values as "string" & 2.0, inside print statements, check for the data type of those variables
 
 # Your code goes below here
 

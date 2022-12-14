8b9c6fc6982aaa299fc98dcb658ff05397926dba
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-14 01:09:53 +0100

    refactored lesson

--- 05-Operators-Assignment-Operator.py
+++ 05-Operators-Assignment-Operator.py
@@ -1,9 +1,10 @@
 # Operators - (Assignment, Arithmetic, Comparison, Boolean/Logical, Bitwise, is & in)
 
-# Assignment Operator - The assignment operator allows us to assign a value to a variable or a vaariable value to another variable eg;
+# Assignment Operator - The assignment operator allows us to assign a value to a variable or a variable value to another variable e.g;
 
 # number_one = 1
 # number_two = number_one + 1
+# print(number_two)
 
 # To pass the test on this challenge, you are required to declare a variable called (greet_one) and assign it to the string "Hello,", and then declare another variable called (greet_two) and assign it's value to be the addition of the first variable and the string " world!"
 

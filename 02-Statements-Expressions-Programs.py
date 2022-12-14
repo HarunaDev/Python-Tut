683526991dfab9c3ccb1b194ff7d1594b680f47e
Haruna Alvin <harunaalvin69@gmail.com>
2022-12-14 01:27:36 +0100

    refactored lesson

--- 02-Statements-Expressions-Programs.py
+++ 02-Statements-Expressions-Programs.py
@@ -2,16 +2,16 @@
 
 # an Expression is any type of code in python that returns a value e.g;
  
-# 1 + 1
-# "Haruna"
+1 + 1
+"Haruna"
 
 # a Statement is an operation on a value, e.g are;
  
-# greet = "Hello, world!" 
+greet = "Hello, world!" 
 
 # a Program is formed by a series of statement like we have above. But those statements can be put in one line but must be separated by a semi-colon e.g;
 
-# greet_two = "Hello, pythonistas!"; print(greet_two)
+greet_two = "Hello, pythonistas!"; # print(greet_two)
 
 # To pass the test on this challenge, you are required to write a program that declares two variables (string_one & string_two) with the string values as ("Bannana", "Apple") & print the two variables, you are required to put them on the same line.
 

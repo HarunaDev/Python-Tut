2dc598da48c0c3d10ee8930b90e270dc03dae04e
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-12 12:14:12 +0100

    added lesson for string char & slicing

---
+++ 18-String-Char-Slicing.py
@@ -0,0 +1,23 @@
+# String Characters & Slicing - You can also get a specific charcater in a string using square brackets e.g;
+
+stringChar = "Hello"
+# print(stringChar[2]) # you can also use a  negative number to start counting at the end
+
+# Slicing - This allows you to specify a range when trying to get a specific character. e.g;
+
+stringChar += ", world"
+# print(stringChar[2:9]) 
+# inside of the square brackets you pass in 2 values separated by a comma, the first is index of where you want to start and then where you want to end.
+
+# To pass the test on this challenge, you are required to look trhough the code below and answer the following statements using an inline comment
+
+checkChar = "We love coding in Python"
+
+# Your code goes below here
+checkChar[5]
+checkChar[-2]
+checkChar[0]
+checkChar[7]
+checkChar[-2:9]
+checkChar[2:11]
+# Your code goes above here

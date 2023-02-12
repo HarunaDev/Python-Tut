2530edd301b56c53915823cc16801f8804491f89
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-12 12:47:01 +0100

    added lesson for boolean

---
+++ 19-Booleans.py
@@ -0,0 +1,27 @@
+# Booleans- (bool type) This data type only returns two values, either True or False e.g;
+
+done = True
+
+if done:
+    print("Yes! I'm done with my Assignment")
+else:
+    print("No, I'm too lazy")  
+
+# When evaluating values that are not True or False e.g other data types, it's nice to know that any value that has something is represented as True and a value with is empty will be False e'g;
+
+checkValue = 2 #This will be True because it has a truthy value
+checkValue2 = "" #This will be False because it is represented by a False value of an empty string
+checkValue3 = -1 #This will be true because a negative value represents a truthy value
+
+# To pass the test on this challenge, you are required to check the following statements using the bool class constructor and type the answer next to it using inline comment.
+
+checkValue4 = -70
+checkValue5 = "-20"
+checkValue6 = "0"
+checkValue7 = 0
+checkValue8 = []
+checkValue9 = "\n"
+
+# Your code goes below here
+
+# Your code goes above here

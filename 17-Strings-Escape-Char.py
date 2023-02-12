2a884d7530607b729e95311efa1a5deb15372fc3
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-12 11:33:49 +0100

    Added lessons on escape char

---
+++ 17-Strings-Escape-Char.py
@@ -0,0 +1,19 @@
+# Strings - Escaping Characters -- You can also escape certain character's in python using the back-slash \ before the cheracter you want to escape, what it means is that when you escape a character, it won't function as it normally would in python, but rather it becomes part of a string. e.g;
+
+escapeChar = 'Let\'s eat, Dad'
+
+escapeChar2 = "examples of quotes you can use in python include '', \"\", and \"\"\" \"\"\" "
+# print(escapeChar)
+# print(escapeChar2)
+
+# You can also use the escape character for special formating, e.g;
+
+# print("let's make a new line \nhere and \nhere")
+
+# To pass the test on this challenge, you are required to print out the types of string quotes that we have in python
+
+# Note: use the escape character for special formating and print each string quote on a new line
+
+# Your code goes below here
+
+# Your code goes above here

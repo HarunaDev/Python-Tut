5e4b472c0a99cdad1dd8ad5156874a5ddae2b215
Alvin Haruna <harunaalvin69@gmail.com>
2023-02-12 11:06:34 +0100

    added lesson on Strings

---
+++ 15-Strings.py
@@ -0,0 +1,26 @@
+# Strings - A string is a set of characters enclosed in double or single quotes, as long as the quotes are the same on both sides. e.g;
+
+string_one = "double quote string"
+string_two = 'single quote string'
+string_example = "please always use " + string_one + "s when coding."
+string_example += " ;)"
+# print(string_example)
+
+# It is good to know that you can make strings multi-line when you enclose them in three quotes on each side e.g;
+
+# print("""this 
+#         is a multi-line
+#         string """)
+
+
+# To pass the test on this challenge, you are required to declare a variable called (quote), and set it's value to be the text below
+
+# Everybody taking this course should learn to program a computer,
+# because it teaches you how to think.
+# - Steve Jobs 
+
+# Note: your quote must be on 3 separate lines as show above.
+
+# Your code goes below here
+
+# Your code goes above here
